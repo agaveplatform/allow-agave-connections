334f367664f89a085a53710ca2dde899f1dc6bc0
Rion Dooley <dooley@tacc.utexas.edu>
2016-07-29 21:23:57 -0500

    Added script to update iptables based on the agave rss feed of server ip addresses and one or more ports passed into the script. Updated pingdom script to do the same.

--- update_agave_connectors.sh
+++ update_agave_connectors.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-# Update the pingdom firewall rules based on their feed
+# Update the agave firewall rules based on their feed
 
-# RSS feed where the current list of pingdom hosts lives.
-PINGDOM_SERVER_FEED='https://www.pingdom.com/rss/probe_servers.xml'
+# RSS feed where the current list of agave hosts lives.
+AGAVE_SERVER_FEED='http://agaveapi.co/server-feed/'
 
-# Location of the cache directory for the pingdom ip addresses
-PINGDOM_IP_CACHE_DIR=pingdom
+# Location of the cache directory for the agave ip addresses
+AGAVE_IP_CACHE_DIR=agave
 
 # Uncomment to force the script to run
 #FORCE_UPDATE=1
 
 # Uncomment to enabled debug output
-FORCE_DEBUG=1
+#FORCE_DEBUG=1
 
 # Uncomment to print actual iptable rules
 #FORCE_TRACE=1
 
 # Uncomment to print the list of ip addresses and exist
 #FETCH_ONLY=1
+
 
 ######################################################
 # DO NOT EDIT BELOW THIS LINE
@@ -36,27 +37,28 @@ fi
 
 echo "Running script to allow Agave servers access to this host on port ${PROTOCOL_PORTS}"
 
-if [[ ! -e "$PINGDOM_IP_CACHE_DIR" ]]; then
-	mkdir -p $PINGDOM_IP_CACHE_DIR
+if [[ ! -e "$AGAVE_IP_CACHE_DIR" ]]; then
+	mkdir -p $AGAVE_IP_CACHE_DIR
 fi
 
-(($FORCE_DEBUG)) && echo "Saving pingdom server ip cache to $PINGDOM_IP_CACHE_DIR/pingdom_ips"
+(($FORCE_DEBUG)) && echo "Saving agave server ip cache to $AGAVE_IP_CACHE_DIR/agave_ips"
 
-# get pingdom ips from their rss feed
-(($FORCE_DEBUG)) && echo "Fetching server list from $PINGDOM_SERVER_FEED"
-wget $PINGDOM_SERVER_FEED -O $PINGDOM_IP_CACHE_DIR/probe_servers.xml -o /dev/null
-if (("$FETCH_ONLY")); then
-	cat $PINGDOM_IP_CACHE_DIR/probe_servers.xml | grep IP | sed -e 's/.*IP: //g' | sed -e 's/; Host.*//g' | grep -v IP
+# get agave ips from their rss feed
+(($FORCE_DEBUG)) && echo "Fetching server list from $AGAVE_SERVER_FEED"
+wget $AGAVE_SERVER_FEED -O $AGAVE_IP_CACHE_DIR/agave_connector_servers.xml -o /dev/null
+if [[ -n "$FETCH_ONLY" ]]; then
+	cat $AGAVE_IP_CACHE_DIR/agave_connector_servers.xml | grep '<title>' | sed -e 's/<title>//g' | sed -e 's/<\/title>//g' | sed -e 's/    //g' | grep -v "Listing"
 	exit 0
 else
-	# rotate pingdom ip list (keep 8 days)
-	LIST=$(ls -r $PINGDOM_IP_CACHE_DIR/pingdom_ips*);
+	# rotate agave ip list (keep 8 days)
+	#
+	LIST=$(ls -r $AGAVE_IP_CACHE_DIR/agave_ips*);
 	for i in $LIST; do
 		# get index of file
 		INDEX=$(ls $i | cut -d"." -f 2)
 
-		# if there's no index, rename to pingdom_ips.0
-		if [ $INDEX = "$PINGDOM_IP_CACHE_DIR/pingdom_ips" ]; then
+		# if there's no index, rename to agave_ips.0
+		if [ $INDEX = "$AGAVE_IP_CACHE_DIR/agave_ips" ]; then
 			NEW=$INDEX.0
 			mv $i $NEW
 		# remove files with index > 6 (keep 8 files)
@@ -70,13 +72,13 @@ else
 		fi
 	done
 
-	cat $PINGDOM_IP_CACHE_DIR/probe_servers.xml | grep IP | sed -e 's/.*IP: //g' | sed -e 's/; Host.*//g' | grep -v IP > $PINGDOM_IP_CACHE_DIR/pingdom_ips
+	cat $AGAVE_IP_CACHE_DIR/agave_connector_servers.xml | grep '<title>' | sed -e 's/<title>//g' | sed -e 's/<\/title>//g' | sed -e 's/    //g' | grep -v "Listing" > $AGAVE_IP_CACHE_DIR/agave_ips
 fi
 
 # if old lists do not exist, just allow all ips in the feed on each of the supplied $PROTOCOL_PORTS
-if [ ! -f $PINGDOM_IP_CACHE_DIR/pingdom_ips.0 ]; then
+if [ ! -f $AGAVE_IP_CACHE_DIR/agave_ips.0 ]; then
 	# iterate over all ip addresses
-	for j in `cat $PINGDOM_IP_CACHE_DIR/pingdom_ips`; do
+	for j in `cat $AGAVE_IP_CACHE_DIR/agave_ips`; do
 		# add a rule for each protocol port given as an argument to this script
 		(($FORCE_DEBUG)) && echo "Allowing access to $j on ${PROTOCOL_PORTS[@]}"
 		for p in "${PROTOCOL_PORTS[@]}"; do
@@ -86,10 +88,10 @@ if [ ! -f $PINGDOM_IP_CACHE_DIR/pingdom_ips.0 ]; then
 	done
 else
 	# if there any differences between previous and current list, synch up the ip list
-	if [[ -n "$FORCE_UPDATE" ]] || [[ -n $(diff -q $PINGDOM_IP_CACHE_DIR/pingdom_ips $PINGDOM_IP_CACHE_DIR/pingdom_ips.0) ]]; then
+	if [[ -n "$FORCE_UPDATE" ]] || [[ -n $(diff -q $AGAVE_IP_CACHE_DIR/agave_ips $AGAVE_IP_CACHE_DIR/agave_ips.0) ]]; then
 
 		# get a list of just the removed ip addresses
-		REMOVED_IP=$(diff --changed-group-format='%>' --unchanged-group-format='' $PINGDOM_IP_CACHE_DIR/pingdom_ips $PINGDOM_IP_CACHE_DIR/pingdom_ips.0)
+		REMOVED_IP=$(diff --changed-group-format='%>' --unchanged-group-format='' $AGAVE_IP_CACHE_DIR/agave_ips $AGAVE_IP_CACHE_DIR/agave_ips.0)
 		if [[ -n "$REMOVED_IP" ]] || (("$FORCE_UPDATE")); then
 			for i in `echo $REMOVED_IP`; do
 				# add a rule for each port given as an argument to this script
@@ -102,10 +104,10 @@ else
 		fi
 
 		# get a list of just the added ip addresses
-		ADDED_IP=$(diff --changed-group-format='%>' --unchanged-group-format='' $PINGDOM_IP_CACHE_DIR/pingdom_ips.0 $PINGDOM_IP_CACHE_DIR/pingdom_ips)
+		ADDED_IP=$(diff --changed-group-format='%>' --unchanged-group-format='' $AGAVE_IP_CACHE_DIR/agave_ips.0 $AGAVE_IP_CACHE_DIR/agave_ips)
 		if [[ -n "$ADDED_IP" ]] || (("$FORCE_UPDATE")); then
 			# add a rule for each ip address added
-			for j in `cat $PINGDOM_IP_CACHE_DIR/pingdom_ips`; do
+			for j in `cat $AGAVE_IP_CACHE_DIR/agave_ips`; do
 				# add a rule for each port given as an argument to this script
 				(($FORCE_DEBUG)) && echo "Allowing access to $j on ${PROTOCOL_PORTS[@]}"
 				for p in "${PROTOCOL_PORTS[@]}"; do
@@ -113,6 +115,10 @@ else
 					iptables -A INPUT -p tcp -s $j -m tcp --dport $p -j ACCEPT
 				done
 			done
+
+			(($FORCE_DEBUG)) && echo "Allowing outbound traffics to established connectionsfrom Agave hosts"
+			(($FORCE_TRACE)) && echo "iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT"
+			iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 		fi
 	fi
 fi
